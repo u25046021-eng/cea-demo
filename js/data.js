e3e34a25b9a62b0ab895639a94a2d4314c37d93a
Albert <u25046021@gmail.com>
2025-11-18 13:50:51 -0300

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,25 @@
+// js/data.js
+
+const eventData = [
+    {
+        day: 21,
+        month: "Nov",
+        title: "Charla: Cambio Climático",
+        info: "18:00 pm - 20:00 pm Virtual (Zoom)",
+        link: "https://www.instagram.com/medioambientestgo/"
+    },
+    {
+        day: 19,
+        month: "Nov",
+        title: "Cuidado de Jardín y Huertas",
+        info: "10:00 hrs - Centro Comunitario (Herrera 360)",
+        link: "https://forms.gle/EJEMPLO"
+    },
+    {
+        day: 26,
+        month: "Nov",
+        title: "Taller de Prendedores",
+        info: "10:00 hrs - Centro Comunitario (Herrera 360)",
+        link: "https://www.instagram.com/medioambientestgo/"
+    }
+];
